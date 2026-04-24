c8d2b4e6501f648522aa9fc2420ad1297fa344b2
JaecobV <23389@burnside.school.nz>
2026-04-24 12:30:47 +1200

    basic flask application changes

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, g
+import sqlite3
+
+DATABASE = 'pc_parts.db'
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row  # allows column names
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
